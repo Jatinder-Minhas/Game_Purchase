d8282f51cbc74432edc0bb6e1a7e692840bc4080
991588302 <minhajai@sheridancollege.ca>
2021-07-01 20:02:55 -0400

    Add project files.

---
+++ Final_Jatinder_Minhas/Controller.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Jatinder_Minhas
+{
+    class Controller
+    {
+        private DatabaseAccess databaseAccess;
+
+        public Controller()
+        {
+            databaseAccess = new DatabaseAccess();
+        }
+
+        private void displayMenu()
+        {
+            Console.WriteLine("Main Menu\n");
+            Console.WriteLine("1. Purchase Game");
+            Console.WriteLine("2. View Customer Transaction History");
+            Console.WriteLine("3. View all Transactions");
+            Console.WriteLine("4. Exit");
+        }
+
+        public void start()
+        {
+            bool loopCheck = true;
+
+            while (loopCheck)
+            {
+                Console.Clear();
+                displayMenu();
+                Console.WriteLine();
+
+                Console.Write("Enter your choice: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Purchase Game\n");
+
+                        Console.Write("The Customer Name: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine();
+
+                        if(!databaseAccess.isCustomerExist(name))
+                        {
+                            databaseAccess.insertCustomer(name);
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"{"Game ID",15} | {"Game Name",-35} | {"Price",12} | {"In Stock",15}");
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        List<Game> games = databaseAccess.getGames();
+                        foreach (Game g in games)
+                        {
+                            Console.WriteLine($"{g.GameId,15} | {g.Name,-35} | {g.Price,12} | {g.Stock,15}");
+                        }
+
+                        bool isGameExist = false;
+
+                        while (!isGameExist)
+                        {
+                            Console.Write("\nEnter Game Id: ");
+                            int gameId = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine();
+
+                            if (games.Exists(g => g.GameId == gameId))
+                            {
+                                isGameExist = true;
+                                Game game = games.Find(g => g.GameId == gameId);
+
+                                int quantity = 0;
+
+                                if (game.Stock != 0)
+                                {
+                                    do
+                                    {
+                                        Console.Write("Enter Quantity(More than 5 = 10% discount): ");
+                                        quantity = Convert.ToInt32(Console.ReadLine());
+
+                                        if (game.Stock >= quantity)
+                                        {
+                                            databaseAccess.placeOrder(databaseAccess.getCustomerByName(name).CustomerId, gameId, quantity);
+
+                                            Console.WriteLine("\nYour order is placed successfully!\n");
+
+                                            Console.ForegroundColor = ConsoleColor.Blue;
+                                            Console.WriteLine($"{"Order ID",15} | {"Date",-25} | {"Customer Name",-25} | {"Game Name",-25} | {"Price",15} | {"Quantity",15} | {"Discount",15} | {"Tax",15} | {"Net Total",15}");
+                                            Console.WriteLine();
+                                            Console.ForegroundColor = ConsoleColor.White;
+
+                                            databaseAccess.displayLatestOrder();
+                                            
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Enter a quantity less than stock!\n");
+                                        }
+                                    }
+                                    while (game.Stock < quantity);
+                                }
+                                else
+                                {
+                                     Console.WriteLine("Sorry, this game is out of stock");
+                                     isGameExist = false;
+                                }
+                                    
+                            }
+                            else
+                            {
+                                Console.WriteLine("Game ID is invalid!");
+                            }
+                        }
+
+                        Console.WriteLine("\nPress Enter to Continue...");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("View Customer Transactions\n");
+
+                        Console.WriteLine("The Customer Names --->\n");
+
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"{"Game ID",15} | {"Game Name",-35}");
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        List<Customer> customers = databaseAccess.getAllCustomers();
+                        foreach (Customer c in customers)
+                        {
+                            Console.WriteLine($"{c.CustomerId,15} | {c.Name,-35}");
+                        }
+
+                        Console.Write("\nEnter the CustomerId: ");
+                        int customerId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+
+                        if(databaseAccess.isCustomerExist(customerId))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"{"Order ID",15} | {"Date",-25} | {"Customer Name",-25} | {"Game Name",-25} | {"Price",15} | {"Quantity",15} | {"Discount",15} | {"Tax",15} | {"Net Total",15}");
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.White;
+
+                            databaseAccess.historyByCustomerId(customerId);
+                        }
+                        else
+                        {
+                            Console.Write("\tCustomer Not Exist. Invalid Customer Id\n");
+                        }
+
+                        Console.WriteLine("\nPress Enter to Continue...");
+                        Console.ReadLine();
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("View All Customers Transactions\n");
+
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"{"Order ID",15} | {"Date",-25} | {"Customer Name",-25} | {"Game Name",-25} | {"Price",15} | {"Quantity",15} | {"Discount",15} | {"Tax",15} | {"Net Total",15}");
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        databaseAccess.historyAll();
+
+                        Console.WriteLine("\nPress Enter to Continue...");
+                        Console.ReadLine();
+
+                        break;
+
+                    case 4:
+                        loopCheck = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Final_Jatinder_Minhas/DatabaseAccess.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Jatinder_Minhas
+{
+    class DatabaseAccess
+    {
+
+        public bool isCustomerExist(string name)
+        {
+            bool isExist = false;
+
+            using (var context = new GameShoppingDBEntities())
+            {
+                var customer = context.Customers
+                          .Where(s => s.Name.ToUpper().Equals(name.ToUpper()))
+                          .FirstOrDefault();
+
+                if (customer == null)
+                    isExist = false;
+                else
+                    isExist = true;
+            }
+            return isExist;
+        }
+
+        public bool isCustomerExist(int customerId)
+        {
+            bool isExist = false;
+
+            using (var context = new GameShoppingDBEntities())
+            {
+                var customer = context.Customers
+                          .Where(c => c.CustomerId == customerId)
+                          .FirstOrDefault();
+
+                if (customer == null)
+                    isExist = false;
+                else
+                    isExist = true;
+            }
+            return isExist;
+        }
+
+        public Customer getCustomerByName(string name)
+        {
+            using (var context = new GameShoppingDBEntities())
+            {
+                var customer = context.Customers
+                          .Where(s => s.Name.ToUpper().Equals(name.ToUpper()))
+                          .FirstOrDefault();
+
+                return customer;
+            }
+        }
+
+        public Customer getCustomerById(int customerId)
+        {
+            using (var context = new GameShoppingDBEntities())
+            {
+                var customer = context.Customers
+                          .Where(c => c.CustomerId == customerId)
+                          .FirstOrDefault();
+
+                return customer;
+            }
+        }
+
+        public List<Customer> getAllCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (var context = new GameShoppingDBEntities())
+            {
+                customers = context.Customers
+                               .ToList();
+            }
+            return customers;
+        }
+
+        public void displayLatestOrder()
+        {
+            using (var context = new GameShoppingDBEntities())
+            {
+                Order order = context.Orders
+                              .OrderByDescending(o => o.OrderId)
+                              .FirstOrDefault();
+
+                if(order != null)
+                {
+                    double tax = ((order.Game.Price * order.Quantity - order.Discount) * 13) / 100;
+                    double total = order.Game.Price * order.Quantity - order.Discount + tax;
+
+                    Console.WriteLine($"{order.OrderId,15} | {order.Date.ToString("dddd, dd MMMM yyyy"),-25} | {order.Customer.Name,-25} | {order.Game.Name,-25} | {"$" + order.Game.Price,15} | {order.Quantity,15} | {"$" + order.Discount,15} | {"$" + tax,15} | {"$" + total,15}");
+                }
+                else
+                {
+                    Console.Write("\tNo Records Found\n");
+                }
+            }
+        }
+
+        public void historyByCustomerId(int customerId)
+        {
+
+            using (var context = new GameShoppingDBEntities())
+            {
+               var orders = context.Orders
+                          .Where(o => o.CustomerId == customerId)
+                          .ToList();
+
+                if (orders.Any())
+                {
+                    foreach (Order o in orders)
+                    {
+
+                        double tax = ((o.Game.Price * o.Quantity - o.Discount) * 13) / 100;
+                        double total = o.Game.Price * o.Quantity - o.Discount + tax;
+
+                        Console.WriteLine($"{o.OrderId,15} | {o.Date.ToString("dddd, dd MMMM yyyy"),-25} | {o.Customer.Name,-25} | {o.Game.Name,-25} | {"$" + o.Game.Price,15} | {o.Quantity,15} | {"$" + o.Discount,15} | {"$" + tax,15} | {"$" + total,15}");
+                    }
+                }
+                else
+                {
+                    Console.Write("\tNo Records Found\n");
+                }
+            }
+        }
+
+        public void historyAll()
+        {
+            using (var context = new GameShoppingDBEntities())
+            {
+                var orders = context.Orders
+                           .ToList();
+
+                if(orders.Any())
+                {
+                    foreach (Order o in orders)
+                    {
+                        double tax = ((o.Game.Price * o.Quantity - o.Discount) * 13) / 100;
+                        double total = o.Game.Price * o.Quantity - o.Discount + tax;
+
+                        Console.WriteLine($"{o.OrderId,15} | {o.Date.ToString("dddd, dd MMMM yyyy"),-25} | {o.Customer.Name,-25} | {o.Game.Name,-25} | {"$" + o.Game.Price,15} | {o.Quantity,15} | {"$" + o.Discount,15} | {"$" + tax,15} | {"$" + total,15}");
+                    }
+                }
+                else
+                {
+                    Console.Write("\tNo Records Found\n");
+                }
+            }
+        }
+
+        public bool insertCustomer(string name)
+        {
+            bool isInserted = false;
+            using (var context = new GameShoppingDBEntities())
+            {
+                var customer = new Customer();
+                customer.Name = name;
+                context.Customers.Add(customer);
+                context.SaveChanges();
+
+                isInserted = isCustomerExist(name);
+            }
+
+            return isInserted;
+        }
+
+        public List<Game> getGames()
+        {
+            List<Game> games = new List<Game>();
+
+            using (var context = new GameShoppingDBEntities())
+            {
+                games = context.Games
+                        .ToList();
+            }
+            return games;
+        }
+
+        public void placeOrder(int customerId, int gameId, int quantity)
+        {
+            using (var context = new GameShoppingDBEntities())
+            {
+                var order = new Order
+                {
+                    Date = DateTime.Now,
+                    Quantity = quantity,
+                    CustomerId = customerId,
+                    GameId = gameId
+                };
+
+                var game = new Game();
+                game = context.Games
+                       .Where(g => g.GameId == order.GameId)
+                       .FirstOrDefault();
+
+                order.Discount = (game.Price * quantity * ((quantity >= 5) ? 10 : 0)/100);
+
+                game.Stock -= quantity;
+
+                context.Orders.Add(order);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
